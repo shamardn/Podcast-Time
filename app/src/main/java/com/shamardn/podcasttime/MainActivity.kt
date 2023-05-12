613c7d36c2d10b36f8ad5cd398ab1d7f33d17e26
SHAMARDN <shamardn@gmail.com>
2023-05-12 22:04:47 +0300

    add splash screen and change app icon

--- app/src/main/java/com/shamardn/podcasttime/MainActivity.kt
+++ app/src/main/java/com/shamardn/podcasttime/MainActivity.kt
@@ -2,10 +2,16 @@ package com.shamardn.podcasttime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.PodcastTimeTheme)
+
         setContentView(R.layout.activity_main)
+
+        installSplashScreen()
     }
 }
