6b3cf7fd44918d8a42aa5a52e774652b5cec2355
SHAMARDN <shamardn@gmail.com>
2023-08-08 03:50:55 +0300

    fix opening bottom sheet from SearchFragment

--- app/src/main/java/com/shamardn/podcasttime/ui/search/SearchFragment.kt
+++ app/src/main/java/com/shamardn/podcasttime/ui/search/SearchFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.shamardn.podcasttime.databinding.FragmentSearchBinding
+import com.shamardn.podcasttime.media.exoplayer.MediaViewModel
 import com.shamardn.podcasttime.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
@@ -22,6 +25,7 @@ class SearchFragment : Fragment(), SearchInteractionListener {
     private lateinit var binding: FragmentSearchBinding
     private lateinit var searchAdapter: SearchAdapter
     private val viewModel: SearchViewModel by viewModels()
+    private val mediaViewModel: MediaViewModel by activityViewModels()
     private var bottomNavigationViewVisibility = View.GONE
 
     private fun setBottomNavigationVisibility() {
@@ -49,6 +53,8 @@ class SearchFragment : Fragment(), SearchInteractionListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        showBottomSheet()
+
         binding.imgSearchBackArrow.setOnClickListener {
             view.findNavController().popBackStack()
         }
@@ -70,5 +76,14 @@ class SearchFragment : Fragment(), SearchInteractionListener {
     override fun onClickPodcast(trackId: Long) {
         val action = SearchFragmentDirections.actionSearchFragmentToPodcastDetailsFragment(trackId)
         Navigation.findNavController(binding.root).navigate(action)
+    }
+
+    private fun showBottomSheet() {
+        mediaViewModel.isBottomSheetOpened.observe(viewLifecycleOwner) {
+            if (it) {
+                val action = SearchFragmentDirections.actionSearchFragmentToEpisodeDetailsBottomSheet()
+                this.findNavController().navigate(action)
+            }
+        }
     }
 }
