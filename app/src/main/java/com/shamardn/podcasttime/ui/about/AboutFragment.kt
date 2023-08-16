c800ba10a80015af4d6e0f501e8f8aa4e2706d40
SHAMARDN <shamardn@gmail.com>
2023-08-17 01:39:38 +0300

    design about screen and make some refactors

--- app/src/main/java/com/shamardn/podcasttime/ui/about/AboutFragment.kt
+++ app/src/main/java/com/shamardn/podcasttime/ui/about/AboutFragment.kt
@@ -5,16 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.shamardn.podcasttime.databinding.FragmentAboutBinding
+import com.shamardn.podcasttime.media.exoplayer.MediaViewModel
+import com.shamardn.podcasttime.ui.main.MainActivity
 
 class AboutFragment : Fragment() {
 
     private lateinit var binding: FragmentAboutBinding
+    private var bottomNavigationViewVisibility = View.GONE
+    private val mediaViewModel: MediaViewModel by activityViewModels()
+
+    private fun setBottomNavigationVisibility() {
+        if (activity is MainActivity) {
+            (activity as MainActivity).setBottomNavigationVisibility(bottomNavigationViewVisibility)
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
         binding = FragmentAboutBinding.inflate(inflater, container, false)
+
+        setBottomNavigationVisibility()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showBottomSheet()
+
+        binding.imgAboutBackArrow.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
+    }
+
+    private fun showBottomSheet() {
+        mediaViewModel.isBottomSheetOpened.observe(viewLifecycleOwner) {
+            if (it) {
+                val action =
+                    AboutFragmentDirections.actionAboutFragmentToEpisodeDetailsBottomSheet()
+                this.findNavController().navigate(action)
+            }
+        }
     }
 }
