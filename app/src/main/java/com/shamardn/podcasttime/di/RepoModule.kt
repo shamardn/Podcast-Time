4c344cb622e8d1102f05cd5fd087deab0e9c79c7
SHAMARDN <shamardn@gmail.com>
2024-10-23 21:29:37 +0300

    update Podcast Repository and Module

--- app/src/main/java/com/shamardn/podcasttime/di/RepoModule.kt
+++ app/src/main/java/com/shamardn/podcasttime/di/RepoModule.kt
@@ -1,21 +1,23 @@
 package com.shamardn.podcasttime.di
 
+import com.shamardn.podcasttime.data.datasource.local.database.dao.DownloadDao
 import com.shamardn.podcasttime.data.datasource.local.database.dao.EpisodeDao
 import com.shamardn.podcasttime.data.datasource.local.database.dao.HistoryDao
+import com.shamardn.podcasttime.data.datasource.local.database.dao.PlaylistDao
 import com.shamardn.podcasttime.data.datasource.local.database.dao.PodcastDao
 import com.shamardn.podcasttime.data.datasource.local.database.dao.SubscriptionsDao
 import com.shamardn.podcasttime.data.datasource.remote.ApiService
-import com.shamardn.podcasttime.data.repo.PodcastRepoImpl
-import com.shamardn.podcasttime.domain.repo.PodcastRepo
+import com.shamardn.podcasttime.data.repo.common.PodcastRepoImpl
+import com.shamardn.podcasttime.domain.repo.common.PodcastRepo
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
+
 @Module
 @InstallIn(SingletonComponent::class)
 object RepoModule {
-
     @Provides
     fun provideRepo(
         apiService: ApiService,
@@ -23,7 +25,9 @@ object RepoModule {
         podcastDao: PodcastDao,
         subscriptionsDao: SubscriptionsDao,
         historyDao: HistoryDao,
+        downloadDao: DownloadDao,
+        playlistDao: PlaylistDao,
     ): PodcastRepo {
-        return PodcastRepoImpl(apiService, episodeDao, podcastDao, subscriptionsDao, historyDao)
+        return PodcastRepoImpl(apiService, episodeDao, podcastDao, subscriptionsDao, historyDao, downloadDao, playlistDao)
     }
 }
