1eab8d81f91d5c6c923d675be95618efb63ef320
SHAMARDN <shamardn@gmail.com>
2024-10-23 22:12:04 +0300

    update GetSubscriptionsUseCase to return Ui state

--- app/src/main/java/com/shamardn/podcasttime/domain/usecase/GetSubscriptionsUseCase.kt
+++ app/src/main/java/com/shamardn/podcasttime/domain/usecase/GetSubscriptionsUseCase.kt
@@ -1,13 +1,16 @@
 package com.shamardn.podcasttime.domain.usecase
 
-import com.shamardn.podcasttime.data.datasource.local.database.entity.PodcastEntity
-import com.shamardn.podcasttime.domain.repo.PodcastRepo
+import com.shamardn.podcasttime.domain.mapper.UiStateSubscriptionMapper
+import com.shamardn.podcasttime.domain.repo.common.PodcastRepo
+import com.shamardn.podcasttime.ui.common.uistate.PodcastUiState
 import javax.inject.Inject
 
 class GetSubscriptionsUseCase @Inject constructor(
     private val podcastRepo: PodcastRepo,
 ) {
-    suspend operator fun invoke(): List<PodcastEntity> {
-        return podcastRepo.getSubscriptions()
+    suspend operator fun invoke(): List<PodcastUiState> {
+        return podcastRepo.getSubscriptions().map {
+            UiStateSubscriptionMapper.toUiState(it)
+        }
     }
 }
