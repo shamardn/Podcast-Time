e63230b874cc51fcd248c0651901b6c105825fd0
SHAMARDN <shamardn@gmail.com>
2024-10-23 22:34:06 +0300

    update GetDownloadedEpisodesUseCase to return ui state

--- app/src/main/java/com/shamardn/podcasttime/domain/usecase/GetDownloadedEpisodesUseCase.kt
+++ app/src/main/java/com/shamardn/podcasttime/domain/usecase/GetDownloadedEpisodesUseCase.kt
@@ -1,13 +1,14 @@
 package com.shamardn.podcasttime.domain.usecase
 
-import com.shamardn.podcasttime.data.datasource.local.database.entity.EpisodeEntity
-import com.shamardn.podcasttime.domain.repo.PodcastRepo
+import com.shamardn.podcasttime.domain.repo.common.PodcastRepo
+import com.shamardn.podcasttime.ui.common.mapper.EpisodeUiStateToDownloadEntityMapper
+import com.shamardn.podcasttime.ui.common.uistate.EpisodeUiState
 import javax.inject.Inject
 
 class GetDownloadedEpisodesUseCase @Inject constructor(
     private val podcastRepo: PodcastRepo,
 ) {
-    suspend operator fun invoke(): List<EpisodeEntity> {
-       return podcastRepo.getDownloadedEpisodes()
+    suspend operator fun invoke(): List<EpisodeUiState> {
+       return podcastRepo.getAllDownloadedEpisodes().map { EpisodeUiStateToDownloadEntityMapper.toEpisodeUiState(it) }
     }
 }
