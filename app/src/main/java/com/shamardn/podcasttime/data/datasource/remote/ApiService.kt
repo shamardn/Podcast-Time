d4cffa65d6fdacf2a89ef0a9643c1e85a11255ca
SHAMARDN <shamardn@gmail.com>
2024-10-27 23:16:29 +0300

    edit api service to get the maximum of podcasts exist or episodes in this podcasts

--- app/src/main/java/com/shamardn/podcasttime/data/datasource/remote/ApiService.kt
+++ app/src/main/java/com/shamardn/podcasttime/data/datasource/remote/ApiService.kt
@@ -7,13 +7,14 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("/search?media=podcast&country=eg")
+    @GET("/search?media=podcast&country=eg&limit=5000")
     suspend fun getPodcasts(
         @Query("term") term: String,
     ): PodcastResponse<PodcastDTO>
 
-    @GET("/lookup?entity=podcastEpisode")
+    @GET("/lookup?entity=podcastEpisode&limit=5000")
     suspend fun getEpisodesById(
         @Query("id") trackId: Long,
     ): PodcastResponse<EpisodeDTO>
+
 }
