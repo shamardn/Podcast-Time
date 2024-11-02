530f4a7c06f924ea9ee09017ce7899c83822f883
SHAMARDN <shamardn@gmail.com>
2024-11-02 22:29:17 +0200

    reduce the limit of end point to 50

--- app/src/main/java/com/shamardn/podcasttime/data/datasource/remote/ApiService.kt
+++ app/src/main/java/com/shamardn/podcasttime/data/datasource/remote/ApiService.kt
@@ -7,12 +7,12 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("/search?media=podcast&country=eg&limit=5000")
+    @GET("/search?media=podcast&country=eg&limit=50&country=eg&limit=50")
     suspend fun getPodcasts(
         @Query("term") term: String,
     ): PodcastResponse<PodcastDTO>
 
-    @GET("/lookup?entity=podcastEpisode&limit=5000")
+    @GET("/lookup?entity=podcastEpisode&limit=50")
     suspend fun getEpisodesById(
         @Query("id") trackId: Long,
     ): PodcastResponse<EpisodeDTO>
